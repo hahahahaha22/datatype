a3171556df0c070cc1b68a8648f39aaf3c11d775
hahahahaha22 <66254778+hahahahaha22@users.noreply.github.com>
2020-06-01 05:07:35 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Firstprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "hello world!";
+            int i = 786;
+            double d = 1.5;
+            decimal dc = 3.5m;
+            float f = 4.5f;
+            long l = 1233345333456L;
+            short s = 22332;
+            byte b = 255;
+            uint UI = 300;
+            ulong UL = 1233345333456L;
+            ushort US = 123;
+            Console.WriteLine(a);
+            Console.WriteLine("intger " + i);
+            Console.WriteLine("double " + d);
+            Console.WriteLine("decimal " + dc);
+            Console.WriteLine("float " + f);
+            Console.WriteLine("long " + l);
+            Console.WriteLine("short " + s);
+            Console.WriteLine("byte " + b);
+            Console.WriteLine("unsiged Int " +UI);
+            Console.WriteLine("unsiged Long " + UL);
+            Console.WriteLine("unsiged short " + US);
+        }
+    }
+}
